5fbe3361e8020482285caab3f9686315c5b45894
wN0NAMEw <150360386+wN0NAMEw@users.noreply.github.com>
2024-06-11 18:13:32 +0700

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,1 +1,198 @@
+/**
+@brief Библиотеки
+*/
 
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+//vector<int> ttm;
+#include <stdlib.h>
+using namespace std;
+#define COOK 1 
+#define BOOK 2
+#define DOOK 3
+#define EOOK 4
+#define TOOK 5
+#define POOK 6
+
+/**
+@brief Структура
+*/
+
+struct Book {
+    string title;
+    string author;
+    int year;
+    string genre;
+};
+
+/**
+@brief Масивный вектор
+*/
+
+vector<Book> library;
+
+/**
+@brief Вывод всего
+@param library - векторный массив
+@param return 0 -возвращает функции 0
+*/
+
+int def_print(const vector<Book>& library) {
+  cout << endl;
+  cout << "------------------------------------------------------" << endl;
+  for (const auto& book : library) {
+      cout << book.title << " " << book.author << " " << book.year << " " << book.genre << endl;
+  }
+  cout << "------------------------------------------------------" << endl;
+  return 0;
+}
+
+/**
+@brief Поиск по названию
+@param title - название книги
+@param return 0 - возвращает функции 0
+*/
+
+int searchByTitle(const string& title) {
+  bool found = false;
+  for (const auto& book : library) {
+      if (book.title == title) {
+          cout << "Найден Фильм: " << book.title << " (Автор: " << book.author << ", Год издания: " << book.year << ", Жанр: " << book.genre << ")" << endl;
+          found = true;
+      }
+  }
+  if (!found) {
+      cout << "Фильм с названием " << title << " не найден." << endl;
+  }
+  return 0;
+}
+
+/**
+@brief Поиск по автору
+@param auth - имя автора
+*/
+
+void searchByAuthor(const string& auth) {
+  bool found = false;
+  for (const auto& book : library) {
+      if (book.author == auth) {
+          if (!found) {
+              cout << "Найдены фильмы авторства " << auth << ":" << endl;
+              found = true;
+          }
+          cout << book.title << " (Год издания: " << book.year << ", Жанр: " << book.genre << ")" << endl;
+      }
+  }
+  if (!found) {
+      cout << "Фильмы авторства " << auth << " не найдены." << endl;
+  }
+}
+
+/**
+@brief Удаление книги
+@param title - название книги
+*/
+
+void removeBook(const string& title) {
+  for (auto it = library.begin(); it != library.end(); ++it) {
+      if (it->title == title) {
+          library.erase(it);
+          cout << "Фильм успешно удален из библиотеки!" << endl;
+          return;
+      }
+  }
+  cout << "Фильм с таким названием не найден в библиотеке." << endl;
+}
+
+/**
+@brief Добавление книги
+@param library - векторный массив
+@param return 0 -возвращает функции 0
+*/
+
+int Film(vector<Book>& library) {
+  Book book1;
+  cout << "Введите название Фильма: ";
+  cin >> book1.title;
+  cout << "Введите автора Фильма: ";
+  cin >> book1.author;
+  cout << "Введите год издания Фильма: ";
+  cin >> book1.year;
+  cout << "Введите жанр Фильма: ";
+  cin >> book1.genre;
+  library.push_back(book1);
+  cout << "Фильм успешно добавлен в библиотеку!" << endl;
+  return 0; 
+}
+
+
+/**
+@brief сортировка по жанру 
+@param library - векторный массив
+*/
+
+void sortLibrary(vector<Book>& library) {
+  sort(library.begin(), library.end(), [](const Book& a, const Book& b) {
+      return a.genre < b.genre;
+  });
+  cout << "Библиотека отсортирована по жанру!" << endl;
+}
+
+
+int tuk(){
+    while (true) {
+        cout << "Выберите действие: " << endl;
+        cout << "1. Поиск фильма по названию" << endl;
+        cout << "2. Поиск фильма по автору" << endl;
+        cout << "3. Удаление Фильма" << endl;
+        cout << "4. Хотите добавить Фильм? " << endl;
+        cout << "5. Вывести всю библиотеку" << endl;
+        cout << "6. Сортировка библиотеки по жанру" << endl;
+        int choice;
+        cin >> choice;
+        switch (choice) {
+            case COOK: {
+                cout << "Введите название Фильма: ";
+                string title;
+                cin >> title;
+                searchByTitle(title);
+                break;
+            }
+            case BOOK: {
+                cout << "Введите автора Фильма: ";
+                string author;
+                cin >> author;
+                searchByAuthor(author);
+                break;
+            }
+            case DOOK: {
+                cout << "Введите название Фильма для удаления: ";
+                string titleToRemove;
+                cin >> titleToRemove;
+                removeBook(titleToRemove);
+                break;
+            }
+            case EOOK: {
+                Film(library);
+                break;
+            }
+            case TOOK: {
+                def_print(library);
+                break;
+            }
+            case POOK: {
+                sortLibrary(library);
+                break;
+            }
+            default: {
+                cout << "Некорректный выбор действия." << endl;
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
